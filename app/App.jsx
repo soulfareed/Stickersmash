09f1b0c26a79a60d911411e540fe3fadddcb7713
Md fareed <fareedzaman501@gmil.com>
2024-08-26 13:35:43 +0530

    starting app

---
+++ app/App.jsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View style={style.container}>
+            <Text>
+                Open up App.js to start working on your app !
+            </Text>
+            <StatusBar style="auto" />
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
