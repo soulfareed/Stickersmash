657dd50e95255d25b1d094ae870a558306ea6565
Md fareed <fareedzaman501@gmail.com>
2024-08-28 01:16:43 +0530

    initial

--- app/index.tsx
+++ app/index.tsx
@@ -12,17 +12,17 @@ const PlaceholderImage = require("../assets/images/background-image.png");
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(PlaceholderImage);
 
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      quality: 1,
-    });
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0]);
-    } else {
-      alert("You did not select any image");
-    }
-  };
+  // const pickImageAsync = async () => {
+  //   let result = await ImagePicker.launchImageLibraryAsync({
+  //     allowsEditing: true,
+  //     quality: 1,
+  //   });
+  //   if (!result.canceled) {
+  //     setSelectedImage(result.assets[0]);
+  //   } else {
+  //     alert("You did not select any image");
+  //   }
+  // };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
