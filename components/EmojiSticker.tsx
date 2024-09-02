6d4b69beb320572db741c947080d8d5f1cff7067
Md fareed <fareedzaman501@gmail.com>
2024-09-02 23:17:49 +0530

    bug updated

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,60 +1,60 @@
-// import { useEffect } from "react";
-// import { View, Image, Text, StyleSheet, ImageStyle } from "react-native";
-// import { Gesture, GestureDetector } from "react-native-gesture-handler";
-// import Animated, {
-//   useAnimatedStyle,
-//   useSharedValue,
-//   withSpring,
-// } from "react-native-reanimated";
+import { useEffect } from "react";
+import { View, Image, Text, StyleSheet, ImageStyle } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
-// type ButtonProps = {
-//   imageSize: number;
-//   stickerSource?: string;
-//   imageStyle?: Animated.AnimateStyle<ImageStyle>;
-// };
+type ButtonProps = {
+  imageSize: number;
+  stickerSource?: string;
+  imageStyle?: Animated.AnimateStyle<ImageStyle>;
+};
 
-// export default function EmojiSticker({
-//   imageSize,
-//   stickerSource,
-// }: ButtonProps) {
-//   if (!stickerSource) {
-//     return null;
-//   }
+export default function EmojiSticker({
+  imageSize,
+  stickerSource,
+}: ButtonProps) {
+  if (!stickerSource) {
+    return null;
+  }
 
-//   const scaleImage = useSharedValue(imageSize);
+  const scaleImage = useSharedValue(imageSize);
 
-//   useEffect(() => {
-//     console.log(stickerSource);
-//     return () => {};
-//   }, [stickerSource]);
+  useEffect(() => {
+    console.log(stickerSource);
+    return () => {};
+  }, [stickerSource]);
 
-//   const doubleTap = Gesture.Tap()
-//     .numberOfTaps(2)
-//     .onStart(() => {
-//       if (scaleImage.value === imageSize) {
-//         scaleImage.value = scaleImage.value * 2;
-//       }
-//     });
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value === imageSize) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    });
 
-//   imageStyle = useAnimatedStyle(() => {
-//     return {
-//       fontSize: withSpring(scaleImage.value),
-//     };
-//   });
-//   return (
-//     <View style={styles.container}>
-//       <GestureDetector gesture={doubleTap}>
-//         <Animated.Text style={[imageStyle, { fontSize: imageSize }]}>
-//           {stickerSource}
-//         </Animated.Text>
-//       </GestureDetector>
-//     </View>
-//   );
-// }
+  imageStyle = useAnimatedStyle(() => {
+    return {
+      fontSize: withSpring(scaleImage.value),
+    };
+  });
+  return (
+    <View style={styles.container}>
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Text style={[imageStyle, { fontSize: imageSize }]}>
+          {stickerSource}
+        </Animated.Text>
+      </GestureDetector>
+    </View>
+  );
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     position: "absolute",
-//     top: 350,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 350,
+  },
+});
